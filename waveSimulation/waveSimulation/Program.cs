b4be76c9301ac258cb7dab6fb986a5834fc52740
Mimlorad <miloradpc@gmail.com>
2018-06-28 19:38:54 +0200

    Za Mihaila
    Dont steal pls

--- waveSimulation/waveSimulation/Program.cs
+++ waveSimulation/waveSimulation/Program.cs
@@ -10,7 +10,33 @@ namespace waveSimulation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("o");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int m = Convert.ToInt32(Console.ReadLine());
+            int[,] matrica = new int[n, m];
+            int[,] matricab = new int[n, m];
+            
+            int t = 0;
+            while(t < 1)
+            {
+                for(int i = 0; i < n; i++)
+                {
+                    for(int j = 0; j < m; j++)
+                    {
+                        matrica[i,j] = Convert.ToInt32(Console.ReadLine());
+                    }
+                }
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < m; j++)
+                    {
+                        Console.Write(matrica[i, j]);
+                        Console.Write(" ");
+                    }
+                    Console.WriteLine();
+                }
+                t++;
+            }
+            Console.WriteLine("end");
         }
     }
 }
