fb69d2200d82df5f59918a1b4a09a4117d479fc3
Mimlorad <miloradpc@gmail.com>
2018-06-28 18:04:33 +0200

    WhereItAllStarted
    dun dun duuuuuuuuuuuun

---
+++ waveSimulation/waveSimulation/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace waveSimulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("o");
+        }
+    }
+}
