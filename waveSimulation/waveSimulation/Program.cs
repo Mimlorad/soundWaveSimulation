ae274f57cad7e08de1c1e9911c98790b245e28c1
Mimlorad <miloradpc@gmail.com>
2018-06-29 19:18:43 +0200

    Moved Vector class to separate .cs file

--- waveSimulation/waveSimulation/Program.cs
+++ waveSimulation/waveSimulation/Program.cs
@@ -4,54 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace waveSimulation
 {
-    class Vector
-    {
-        private double x;
-        private double y;
-
-        public Vector(double x, double y)
-        {
-            this.x = x;
-            this.y = y;
-        }
-
-        public Vector()
-        {
-            this.x = 0; this.y = 0;
-        }
-
-        public double GetX()
-        {
-            return this.x;
-        }
-
-        public double GetY()
-        {
-            return this.y;
-        }
-
-        public void SetX(double x)
-        {
-            this.x = x;
-        }
-
-        public void SetY(double y)
-        {
-            this.y = y;
-        }
-    }
-
     class Program
     {
-        Vector SkalProizvod(Vector vec, double scl)
-        {
-            return new Vector(vec.GetX()*scl, vec.GetY()*scl);
-        }
+        
 
         double Izvod(double x, double y)
         {
+
             return 0;
         }
 
